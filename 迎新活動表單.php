b97c0eb19e900b25e8310d3b286cd325f9809295
hsinyu <a1123305@mail.nuk.edu.tw>
2025-02-25 20:37:56 +0800

    Add files via upload

---
+++ 迎新活動表單.php
@@ -0,0 +1,62 @@
+<html>
+    <head>
+        <title>迎新活動行程與報名表</title>
+        <meta charset="utf-8">
+    </head>
+
+    <body text="black">
+        <h1><font face="標楷體">資管迎新行程</font></h1>
+
+        <font face="標楷體" size="4">
+            <table border="1">
+                <tr><td><b>Day1</b></td><td>行程&地點</td><td>備註</td><tr>
+                <tr><td>07:30 - 07:50</td><td>集合報到</td><td>高雄大學管院前(確認人數)</td><tr>
+                <tr><td>08:00</td><td>搭乘遊覽車前往墾丁</td><td></td><tr>
+                <tr><td>11:00 - 12:30</td><td>吃午餐</td><td>波波廚房(義式料理)</td><tr>
+                <tr><td>13:00 - 14:30</td><td>龍磐公園</td><td>超美海景，團體大合照</td><tr>
+                <tr><td>15:00 - 17:30</td><td>白沙灣海邊玩水+夕陽</td><td>裝備提醒：帶泳衣、毛巾、拖鞋、換洗衣物、防曬乳！</td><tr>
+                <tr><td>18:00 - 18:30</td><td>入住住宿</td><td>阿信露營區</td><tr>
+                <tr><td>18:40 - 21:30</td><td>墾丁大街自由活動</td><td></td><tr>
+                <tr><td>21:30 - 21:40</td><td>回集合點</td><td>清點人數</td><tr>
+                <tr><td>21:50</td><td>回露營區</td><td></td><tr>
+            </table>
+            <BR>
+            <table border="1">
+                <tr><td><b>Day2</b></td><td>行程&地點</td><td>備註</td><tr>
+                <tr><td>08:00 - 09:00</td><td>吃早餐</td><td>前一晚統一訂餐</td><tr>
+                <tr><td>09:00 - 09:30</td><td>收拾行李+集合</td><td></td><tr>
+                <tr><td>09:30 - 12:00</td><td>搭車前往台東</td><td></td><tr>
+                <tr><td>12:00 - 13:30</td><td>吃午餐</td><td>台東美食家合菜館</td><tr>
+                <tr><td>14:00 - 15:00</td><td>台東森林公園</td><td>腳踏車環湖</td><tr>
+                <tr><td>15:30 - 16:30</td><td>鐵花村音樂聚落</td><td></td><tr>
+                <tr><td>17:00 - 18:00</td><td>入住台東民宿</td><td>台東慢漫民宿</td><tr>
+                <tr><td>18:00 - 21:30</td><td>迎新晚會 + 烤肉派對</td><td>迎新儀式&團康活動</td><tr>
+                <tr><td>22:00</td><td>回房休息</td><td></td><tr>
+            </table>
+            <BR>
+            <table border="1">
+                <tr><td><b>Day3</b></td><td>行程&地點</td><td>備註</td><tr>
+                <tr><td>08:30 - 09:30</td><td>吃早餐</td><td>前一晚統一訂餐</td><tr>
+                <tr><td>09:30 - 10:00</td><td>收拾行李+集合</td><td></td><tr>
+                <tr><td>10:00 - 11:30</td><td>加路蘭海岸or鹿野高台</td><td></td><tr>
+                <tr><td>12:00 - 13:30</td><td>吃午餐</td><td>稻飯田台東館</td><tr>
+                <tr><td>13:40 - 16:50</td><td>搭車回高雄</td><td></td><tr>
+                <tr><td>17:00</td><td>抵達高雄大學</td><td></td><tr>
+            </table>
+            <BR>
+
+            <BR>
+            報名表單<BR>
+            <form action="" method="">
+                姓名：<input type="text"><BR>
+                參加意願(是或否)：<input type="text"><BR>
+                連絡電話：<input type="text"><BR>
+                緊急聯絡人：<input type="text"><BR>
+                緊急連絡人電話：<input type="text"><BR>
+                有什麼問題or建議：<BR>
+                <textarea cols="40" rows="10"></textarea><BR>
+                <input type="submit"><input type="reset">
+            </form>
+        </font>
+    </body>
+</html>
